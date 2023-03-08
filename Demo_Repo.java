d8e4ad14283c305f4b25e68272b6fb5f53525e47
Kshitija078 <kshitijatakle3854@gmail.com>
2023-03-08 19:48:58 +0530

    Deleted files from feature branch

--- Demo_Repo.java
+++
@@ -1,9 +0,0 @@
-ls
-cal
-date 
-pwd
-man 
-ls -l
-grep
-tac
-head
--- Evenodd.java
+++
@@ -1,9 +0,0 @@
-!# /bin/bash
-
-echo "enter a number to check even or odd"
-read Num
-if (($Num % 2==0))
-then 
-	echo " $Num is even number"
-else
-	echo  "$Num is odd number"
